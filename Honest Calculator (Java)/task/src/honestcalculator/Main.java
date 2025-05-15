d264ef27a86a5d39fda3d14218b4113383ec5bec
Moha <mimohadinho@gmail.com>
2025-05-15 14:57:15 +0200

    Stage 3/5: Total recall

--- Honest Calculator (Java)/task/src/honestcalculator/Main.java
+++ Honest Calculator (Java)/task/src/honestcalculator/Main.java
@@ -3,10 +3,15 @@ package honestcalculator;
 import java.util.Scanner;
 
 public class Main {
+    private static float memory = 0;
+
     private final static String msg_0 = "Enter an equation";
     private final static String msg_1 = "Do you even know what numbers are? Stay focused!";
     private final static String msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?";
     private final static String msg_3 = "Yeah... division by zero. Smart move...";
+    private final static String msg_4 = "Do you want to store the result? (y / n):";
+    private final static String msg_5 ="Do you want to continue calculations? (y / n):";
+
     public static void main(String[] args) {
         // write your code here
         calculator();
@@ -15,17 +20,64 @@ public class Main {
     private static void calculator() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            //Output msg and user input
             System.out.println(msg_0);
             String input = scanner.nextLine();
-            boolean valid = isValid(input);
-            if(valid){
-               operation(input);
-               break;
+
+            //If input contains M use the memory if not negative
+            boolean matched = input.contains("M");
+            boolean matchedSign = input.contains("/");
+            if (matchedSign && memory == 0){
+                System.out.println(msg_3);
+                continue;
+            }else if (matched && memory >= 0) {
+                //Calculate using saved number
+                input = input.replace("M", "" + memory);
+            }else if (matched && memory < 0) {
+                //Otherwise continue
+                continue;
+            }
+
+            //check the validity of the input
+            boolean valid = !input.isEmpty() && isValid(input);
+            if (!valid) continue;
+
+            //Calculations begins
+            float result = processAction(input);
+            System.out.println(result);
+
+            //Storing the results
+            System.out.println("Do you want to store the result? (y / n):");
+            String choice = scanner.nextLine();
+            storeResult(result, choice);
+
+            //Continue Calculating
+            System.out.println("Do you want to continue calculations? (y / n):");
+            String choice1 = scanner.nextLine();
+            boolean continueCal = continueCalculation(choice1);
+
+            //User doesn't want to continue
+            if(!continueCal){
+                break;
             }
         }
     }
 
-    private static void operation(String input) {
+
+    private static void storeResult(float result, String choice) {
+        if(choice.equals("y")) {
+            if (result > 0) {
+                memory = result;
+            }
+        }
+    }
+
+    private static boolean continueCalculation(String choice) {
+        return choice.equals("y");
+    }
+
+
+    private static float processAction(String input) {
         String replaceInput = input.replace(" ", "");
         String[] parts = replaceInput.split("[-+*/]");
 
@@ -33,39 +85,19 @@ public class Main {
         float y = Float.parseFloat(parts[1]);
         String oper = replaceInput.replaceAll("[\\d]*\\.*","");
 
-        switch (oper){
-            case "+":
-                addition(x,y);
-                break;
-            case "-":
-                substraction(x,y);
-                break;
-            case "*":
-                multiplication(x,y);
-                break;
-            case "/":
-                division(x,y);
-                break;
-            default:
-                System.out.println("Invalid operator!");
-        }
+        return operation(x,oper,y);
     }
 
-    private static void addition(float x, float y) {
-        System.out.println(x+y);
+    private static float operation(float x, String oper, float y) {
+        return switch (oper) {
+            case "+" -> x + y;
+            case "-" -> x - y;
+            case "*" -> x * y;
+            case "/" -> x / y;
+            default -> 0;
+        };
     }
 
-    private static void substraction(float x,float y) {
-        System.out.println(x-y);
-    }
-
-    private static void multiplication(float x, float y) {
-        System.out.println(x*y);
-    }
-
-    private static void division(float x, float y) {
-        System.out.println(x/y);
-    }
 
     private static boolean isValid (String input){
             if (input.matches("[0-9]\\.?[0-9]? [a-z!@\\s.] [0-9]\\.?[0-9]?")) {
