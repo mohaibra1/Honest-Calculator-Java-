13f835e448c1a76b3231f51f747ba5a456997b91
Moha <mimohadinho@gmail.com>
2025-05-15 11:37:17 +0200

    Stage 2/5: First calculations

--- Honest Calculator (Java)/task/src/honestcalculator/Main.java
+++ Honest Calculator (Java)/task/src/honestcalculator/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private final static String msg_0 = "Enter an equation";
     private final static String msg_1 = "Do you even know what numbers are? Stay focused!";
     private final static String msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?";
-
+    private final static String msg_3 = "Yeah... division by zero. Smart move...";
     public static void main(String[] args) {
         // write your code here
         calculator();
@@ -19,18 +19,70 @@ public class Main {
             String input = scanner.nextLine();
             boolean valid = isValid(input);
             if(valid){
-                break;
+               operation(input);
+               break;
             }
         }
     }
 
-        private static boolean isValid (String input){
-            if (input.matches("[0-9]\\.?[0-9]? [a-z] [0-9]\\.?[0-9]?")) {
+    private static void operation(String input) {
+        String replaceInput = input.replace(" ", "");
+        String[] parts = replaceInput.split("[-+*/]");
+
+        float x = Float.parseFloat(parts[0]);
+        float y = Float.parseFloat(parts[1]);
+        String oper = replaceInput.replaceAll("[\\d]*\\.*","");
+
+        switch (oper){
+            case "+":
+                addition(x,y);
+                break;
+            case "-":
+                substraction(x,y);
+                break;
+            case "*":
+                multiplication(x,y);
+                break;
+            case "/":
+                division(x,y);
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+    }
+
+    private static void addition(float x, float y) {
+        System.out.println(x+y);
+    }
+
+    private static void substraction(float x,float y) {
+        System.out.println(x-y);
+    }
+
+    private static void multiplication(float x, float y) {
+        System.out.println(x*y);
+    }
+
+    private static void division(float x, float y) {
+        System.out.println(x/y);
+    }
+
+    private static boolean isValid (String input){
+            if (input.matches("[0-9]\\.?[0-9]? [a-z!@\\s.] [0-9]\\.?[0-9]?")) {
                 System.out.println(msg_2);
                 return false;
-            } else if (input.matches("[a-z] [-|+|*] [0-9]?\\.?[0-9]?") ||
-            input.matches("[0-9]?\\.?[0-9]? [-|+|*] [a-z]")) {
+            } else if (input.matches("[a-z] [-+*] [0-9]?\\.?[0-9]?") ||
+            input.matches("[0-9]?\\.?[0-9]? [-+*] [a-z]")) {
                 System.out.println(msg_1);
+                return false;
+            }else if (input.matches("[a-z] [-+*/] [a-z]")){
+                System.out.println(msg_1);
+                return false;
+            }else if(input.matches("[0-9]\\.?[]0-9]? ?\\\\ ?[0-9]\\.?[0-9]?")){
+                System.out.println(msg_1);
+                return false;
+            }else if(input.matches("[0-9]\\.?[0-9]? ?/ ?0")){
+                System.out.println(msg_3);
                 return false;
             }
             return true;
