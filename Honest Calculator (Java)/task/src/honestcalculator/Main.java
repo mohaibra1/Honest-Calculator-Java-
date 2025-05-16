7a9a8b0f6d811c37e67ec4e3f30400a4d83f8aec
Moha <mimohadinho@gmail.com>
2025-05-16 14:21:21 +0200

    Stage 4/5: The laziness test

--- Honest Calculator (Java)/task/src/honestcalculator/Main.java
+++ Honest Calculator (Java)/task/src/honestcalculator/Main.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     private static float memory = 0;
-
+    private static boolean stop = true;
     private final static String msg_0 = "Enter an equation";
     private final static String msg_1 = "Do you even know what numbers are? Stay focused!";
     private final static String msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?";
     private final static String msg_3 = "Yeah... division by zero. Smart move...";
     private final static String msg_4 = "Do you want to store the result? (y / n):";
     private final static String msg_5 ="Do you want to continue calculations? (y / n):";
+    private final static String msg_6 = " ... lazy";
+    private final static String msg_7 = " ... very lazy";
+    private final static String msg_8 = " ... very, very lazy";
+    private final static String msg_9 = "You are";
 
     public static void main(String[] args) {
         // write your code here
@@ -24,40 +28,15 @@ public class Main {
             System.out.println(msg_0);
             String input = scanner.nextLine();
 
-            //If input contains M use the memory if not negative
-            boolean matched = input.contains("M");
-            boolean matchedSign = input.contains("/");
-            if (matchedSign && memory == 0){
-                System.out.println(msg_3);
-                continue;
-            }else if (matched && memory >= 0) {
-                //Calculate using saved number
-                input = input.replace("M", "" + memory);
-            }else if (matched && memory < 0) {
-                //Otherwise continue
-                continue;
-            }
-
             //check the validity of the input
             boolean valid = !input.isEmpty() && isValid(input);
             if (!valid) continue;
 
             //Calculations begins
-            float result = processAction(input);
-            System.out.println(result);
-
-            //Storing the results
-            System.out.println("Do you want to store the result? (y / n):");
-            String choice = scanner.nextLine();
-            storeResult(result, choice);
-
-            //Continue Calculating
-            System.out.println("Do you want to continue calculations? (y / n):");
-            String choice1 = scanner.nextLine();
-            boolean continueCal = continueCalculation(choice1);
+            processAction(input);
 
             //User doesn't want to continue
-            if(!continueCal){
+            if(!stop){
                 break;
             }
         }
@@ -72,30 +51,86 @@ public class Main {
         }
     }
 
-    private static boolean continueCalculation(String choice) {
-        return choice.equals("y");
-    }
 
+    private static void processAction(String input) {
+        //If input contains M use the memory if not negative
+        boolean matched = input.contains("M");
+        //boolean matchedSign = input.contains("/");
+        if (matched) {
+            input = input.replace("M", "" + memory);
+        }
 
-    private static float processAction(String input) {
         String replaceInput = input.replace(" ", "");
         String[] parts = replaceInput.split("[-+*/]");
 
-        float x = Float.parseFloat(parts[0]);
-        float y = Float.parseFloat(parts[1]);
+        double x = Double.parseDouble(parts[0]);
+        double y = Double.parseDouble(parts[1]);
         String oper = replaceInput.replaceAll("[\\d]*\\.*","");
-
-        return operation(x,oper,y);
+        check(parts[0], oper, parts[1]);
+        float result = operation(x,oper,y);
+        if(y == 0 && oper.equals("/")) {
+            return;
+        }else{
+            System.out.println(result);
+            continueCalculating(result);
+        }
     }
 
-    private static float operation(float x, String oper, float y) {
-        return switch (oper) {
+    private static void continueCalculating(float result) {
+        Scanner scanner = new Scanner(System.in);
+        //Storing the results
+        System.out.println(msg_4);
+        String choice = scanner.nextLine();
+        storeResult(result, choice);
+
+        //Continue Calculating
+        System.out.println(msg_5);
+        String choice1 = scanner.nextLine();
+        stop = choice1.equals("y");;
+    }
+
+    private static void check(String x, String oper, String y) {
+        String msg = "";
+        if(x.matches("^[0-9]*\\.0$")){
+            x = x.replace(".0", "");
+        }if(y.matches("^[0-9]*\\.0$")){
+            y = y.replace(".0", "");
+        }
+        if(is_one_digit(x) && is_one_digit(y)){
+            msg = msg + msg_6;
+        }if((x.equals("1") || y.equals("1")) && oper.equals("*")){
+            msg = msg + msg_7;
+        }if((x.equals("0") || y.equals("0")) && (oper.equals("*") || oper.equals("+") || oper.equals("-"))){
+            msg = msg + msg_8;
+        }if(!msg.isEmpty()){
+            msg = msg_9 + msg;
+            System.out.println(msg);
+        }
+
+    }
+
+    private static float operation(double x, String oper, double y) {
+        if(y == 0 && oper.equals("/")){
+            System.out.println(msg_3);
+            return 0;
+        }
+        return (float) switch (oper) {
             case "+" -> x + y;
             case "-" -> x - y;
             case "*" -> x * y;
             case "/" -> x / y;
             default -> 0;
         };
+
+    }
+
+    private static boolean is_one_digit(String digit){
+        try{
+            int x = Integer.parseInt(digit);
+            return x > -10 && x < 10;
+        }catch (NumberFormatException ex){
+            return false;
+        }
     }
 
 
@@ -113,10 +148,11 @@ public class Main {
             }else if(input.matches("[0-9]\\.?[]0-9]? ?\\\\ ?[0-9]\\.?[0-9]?")){
                 System.out.println(msg_1);
                 return false;
-            }else if(input.matches("[0-9]\\.?[0-9]? ?/ ?0")){
-                System.out.println(msg_3);
-                return false;
             }
+//            else if(input.matches("[0-9]\\.?[0-9]? ?/ ?0")){
+//                System.out.println(msg_3);
+//                return false;
+//            }
             return true;
         }
 }
